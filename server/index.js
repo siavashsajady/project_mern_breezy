f900e5500fcd8fd47c8b2fc1a8b4be58c76bcb7d
siavashsajady <hubgitpro@gmail.com>
2022-04-08 14:05:19 +0200

    fixed bug

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.pez4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pez4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
