1bdca44617ab3975973335fb025deea049d4fced
siavashsajady <hubgitpro@gmail.com>
2022-04-26 17:37:04 +0200

    implement pagination

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -30,10 +30,6 @@ const Home = () => {
   const searchQuery = query.get('searchQuery');
   const classes = useStyles();
 
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   const searchPost = () => {
     if (search.trim()) {
       dispatch(getPostsBySearch({ search }));
@@ -87,7 +83,7 @@ const Home = () => {
             </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper elevation={6}>
-              <Pagination />
+              <Pagination page={page} />
             </Paper>
           </Grid>
         </Grid>
--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Pagination, PaginationItem } from '@material-ui/lab';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPosts } from '../../actions/posts';
 
 import useStyles from './styles';
 
-const Paginate = () => {
+const Paginate = ({ page }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (page) dispatch(getPosts(page));
+  }, [page]);
 
   return (
     <Pagination
