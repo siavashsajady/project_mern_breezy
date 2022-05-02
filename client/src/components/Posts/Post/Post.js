37cc07bb8fa7cadc7c65d1aeaf5970c2474e8cdb
siavashsajady <hubgitpro@gmail.com>
2022-05-02 08:02:22 +0200

    fixed bug

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -55,7 +55,7 @@ const Post = ({ post, setCurrentId }) => {
   const openPost = () => navigate(`/posts/${post._id}`);
   return (
     <Card className={classes.card} raised elevation={6}>
-      <ButtonBase className={classes.cardActions} onClick={openPost}>
+      <ButtonBase className={classes.cardAction} onClick={openPost}>
         <CardMedia
           className={classes.media}
           image={post.selectedFile}
